c4a4731f9c4da1cdf7a8c662020a347784622863
ina <naleeyoun@naver.com>
2022-02-03 16:48:18 +0900

    자바프로젝트

--- src/com/domain/lecture/ch02/Hello.java
+++ src/com/domain/lecture/ch02/Hello.java
@@ -4,5 +4,14 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("안녕하세요!! 자바 첫 수업시간입니다.");
+		
+		int a = 1;	// 선언 대입(초기화)
+		int b = 2;
+		
+		int c = 0;
+		
+		c = a + b;
+		
+		System.out.println(c);
 	}
 }
